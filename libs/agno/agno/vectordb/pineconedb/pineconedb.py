299827b13035b715fb7c1051a72667cbb2af5d2e
Priti <mail2pritipriya@gmail.com>
2025-02-18 14:48:53 +0530

    Add pinecone compatible version warning (#2154)
    ## Description
    
    - **Summary of changes**: Adds Pinecone version warning

--- libs/agno/agno/vectordb/pineconedb/pineconedb.py
+++ libs/agno/agno/vectordb/pineconedb/pineconedb.py
@@ -1,10 +1,25 @@
 from typing import Any, Dict, List, Optional, Union
 
 try:
+    from packaging import version
+    from pinecone import __version__
+
+    if version.parse(__version__).major >= 6:
+        import warnings
+
+        warnings.warn(
+            "We do not yet support Pinecone v6.x.x. We are actively working to achieve compatibility. "
+            "In the meantime, we recommend using Pinecone v5.4.2 for the best experience. Please run `pip install pinecone==5.4.2`",
+            UserWarning,
+        )
+        raise RuntimeError("Incompatible Pinecone version detected. Execution halted.")
+
     from pinecone import Pinecone, PodSpec, ServerlessSpec
     from pinecone.config import Config
+
 except ImportError:
     raise ImportError("The `pinecone` package is not installed, please install using `pip install pinecone`.")
+
 
 from agno.document import Document
 from agno.embedder import Embedder
