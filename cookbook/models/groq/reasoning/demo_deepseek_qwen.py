d3cceaf34db84f57d694be3f446a976aaa80a201
Dirk Brand <51947788+dirkbrnd@users.noreply.github.com>
2025-02-19 12:52:17 +0200

    Add deepseek and qwen demo cookbook (#2168)
    ## Description
    
    Add demo cookbook to show the new Deepseek-R1-Distill models on Groq
    
    
    https://groq.com/groqcloud-now-offers-qwen-2-5-32b-and-deepseek-r1-distill-qwen-32b/

---
+++ cookbook/models/groq/reasoning/demo_deepseek_qwen.py
@@ -0,0 +1,19 @@
+"""Run `pip install duckduckgo-search sqlalchemy pgvector pypdf openai groq` to install dependencies."""
+
+from agno.agent import Agent
+from agno.knowledge.pdf_url import PDFUrlKnowledgeBase
+from agno.models.groq import Groq
+from agno.vectordb.pgvector import PgVector
+
+db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+
+agent_with_reasoning = Agent(
+    model=Groq(id="Qwen-2.5-32b"),
+    reasoning=True,
+    reasoning_model=Groq(
+        id="Deepseek-r1-distill-qwen-32b", temperature=0.6, max_tokens=1024, top_p=0.95
+    ),
+    show_tool_calls=True,
+    debug_mode=True
+)
+agent_with_reasoning.print_response("9.11 and 9.9 -- which is bigger?", markdown=True)
--- libs/agno/agno/agent/agent.py
+++ libs/agno/agno/agent/agent.py
@@ -2733,7 +2733,7 @@ class Agent:
         # If a reasoning model is provided, use it to generate reasoning
         if reasoning_model_provided:
             # Use DeepSeek for reasoning
-            if reasoning_model.__class__.__name__ == "DeepSeek" and reasoning_model.id == "deepseek-reasoner":
+            if reasoning_model.__class__.__name__ == "DeepSeek" and reasoning_model.id.lower() == "deepseek-reasoner":
                 from agno.reasoning.deepseek import get_deepseek_reasoning, get_deepseek_reasoning_agent
 
                 ds_reasoning_agent = self.reasoning_agent or get_deepseek_reasoning_agent(
@@ -2752,7 +2752,7 @@ class Agent:
                     reasoning_agent_messages=[ds_reasoning_message],
                 )
             # Use Groq for reasoning
-            elif reasoning_model.__class__.__name__ == "Groq" and "deepseek" in reasoning_model.id:
+            elif reasoning_model.__class__.__name__ == "Groq" and "deepseek" in reasoning_model.id.lower():
                 from agno.reasoning.groq import get_groq_reasoning, get_groq_reasoning_agent
 
                 groq_reasoning_agent = self.reasoning_agent or get_groq_reasoning_agent(
