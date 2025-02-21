8dc32ce8868ba7d81c873ba7e631dc662385e5d6
Manthan Gupta <42516515+manthanguptaa@users.noreply.github.com>
2025-02-21 15:05:53 +0530

    Add tests for azure (#2199)
    ## Description
    Add tests for azure model
    
    ---
    
    ## Type of change
    
    Please check the options that are relevant:
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Model update (Addition or modification of models)
    - [ ] Other (please describe):
    
    ---
    
    ## Checklist
    
    - [ ] Adherence to standards: Code complies with Agno’s style guidelines
    and best practices.
    - [ ] Formatting and validation: You have run `./scripts/format.sh` and
    `./scripts/validate.sh` to ensure code is formatted and linted.
    - [ ] Self-review completed: A thorough review has been performed by the
    contributor(s).
    - [ ] Documentation: Docstrings and comments have been added or updated
    for any complex logic.
    - [ ] Examples and guides: Relevant cookbook examples have been included
    or updated (if applicable).
    - [ ] Tested in a clean environment: Changes have been tested in a clean
    environment to confirm expected behavior.
    - [ ] Tests (optional): Tests have been added or updated to cover any
    new or changed functionality.
    
    ---
    
    ## Additional Notes
    
    Include any deployment notes, performance implications, security
    considerations, or other relevant information (e.g., screenshots or logs
    if applicable).
    
    ---------
    
    Co-authored-by: Dirk Brand <51947788+dirkbrnd@users.noreply.github.com>

---
+++ libs/agno/tests/integration/models/azure/ai_foundry/test_multimodal.py
@@ -0,0 +1,17 @@
+from agno.agent.agent import Agent
+from agno.media import Image
+from agno.models.azure import AzureAIFoundry
+
+
+def test_image_input():
+    agent = Agent(
+        model=AzureAIFoundry(id="Phi-4"), markdown=True, telemetry=False, monitoring=False
+    )
+
+    response = agent.run(
+        "Tell me about this image.",
+        images=[Image(url="https://upload.wikimedia.org/wikipedia/commons/0/0c/GoldenGateBridge-001.jpg")],
+    )
+
+    assert "golden" in response.content.lower()
+    assert "bridge" in response.content.lower()
---
+++ libs/agno/tests/integration/models/azure/openai/test_multimodal.py
@@ -0,0 +1,17 @@
+from agno.agent.agent import Agent
+from agno.media import Image
+from agno.models.azure import AzureOpenAI
+
+
+def test_image_input():
+    agent = Agent(
+        model=AzureOpenAI(id="gpt-4o-mini"), markdown=True, telemetry=False, monitoring=False
+    )
+
+    response = agent.run(
+        "Tell me about this image.",
+        images=[Image(url="https://upload.wikimedia.org/wikipedia/commons/0/0c/GoldenGateBridge-001.jpg")],
+    )
+
+    assert "golden" in response.content.lower()
+    assert "bridge" in response.content.lower()
