d9a946e25c47ba3a3ff869db06883a1d487b31e4
YuYan <53218750+allenli178@users.noreply.github.com>
2025-02-27 07:12:20 +0800

    add autogen performance test (#2223)
    ## Description
    
    - **Summary of changes**: Describe the key changes in this PR and their
    purpose.
    - **Related issues**: Mention if this PR fixes or is connected to any
    issues.
    - **Motivation and context**: Explain the reason for the changes and the
    problem they solve.
    - **Environment or dependencies**: Specify any changes in dependencies
    or environment configurations required for this update.
    - **Impact on metrics**: (If applicable) Describe changes in any metrics
    or performance benchmarks.
    
    Fixes # (issue)
    
    ---
    
    ## Type of change
    
    Please check the options that are relevant:
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Model update (Addition or modification of models)
    - [ ] Other (please describe):
    ## agno
    
    
    ![image](https://github.com/user-attachments/assets/e0b92234-8554-47db-9f18-d395906c4c00)
    
    ## autogen
    
    
    ![image](https://github.com/user-attachments/assets/48ecdea1-fcc7-4f30-bcc7-8dc6a70cce1f)
    
    ---
    
    The performance is superb.
    
    
    ## Checklist
    
    - [ ] Adherence to standards: Code complies with Agno’s style guidelines
    and best practices.
    - [ ] Formatting and validation: You have run `./scripts/format.sh` and
    `./scripts/validate.sh` to ensure code is formatted and linted.
    - [ ] Self-review completed: A thorough review has been performed by the
    contributor(s).
    - [ ] Documentation: Docstrings and comments have been added or updated
    for any complex logic.
    - [ ] Examples and guides: Relevant cookbook examples have been included
    or updated (if applicable).
    - [ ] Tested in a clean environment: Changes have been tested in a clean
    environment to confirm expected behavior.
    - [ ] Tests (optional): Tests have been added or updated to cover any
    new or changed functionality.
    
    ---
    
    ## Additional Notes
    
    Include any deployment notes, performance implications, security
    considerations, or other relevant information (e.g., screenshots or logs
    if applicable).
    
    Co-authored-by: Ashpreet <ashpreetbedi@users.noreply.github.com>

---
+++ evals/performance/other/autogen_instantiation.py
@@ -0,0 +1,43 @@
+"""Run `pip install memory_profiler autogen-agentchat "autogen-ext[openai]"` to install dependencies."""
+
+from typing import Literal
+
+from autogen_agentchat.agents import AssistantAgent
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+
+from agno.eval.perf import PerfEval
+
+
+def get_weather(city: Literal["nyc", "sf"]):
+    """Use this to get weather information."""
+    if city == "nyc":
+        return "It might be cloudy in nyc"
+    elif city == "sf":
+        return "It's always sunny in sf"
+    else:
+        raise AssertionError("Unknown city")
+
+
+tools = [get_weather]
+
+
+def instantiate_agent():
+    return AssistantAgent(
+        name="assistant",
+        model_client=OpenAIChatCompletionClient(
+            model="gpt-4o",
+            model_info={
+                "vision": False,
+                "function_calling": True,
+                "json_output": False,
+                "family": "gpt-4o",
+            },
+        ),
+        tools=tools,
+    )
+
+
+autogen_instantiation = PerfEval(func=instantiate_agent, num_iterations=1000)
+
+if __name__ == "__main__":
+    autogen_instantiation.run(print_results=True)
