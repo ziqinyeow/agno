9bc4c795dc958199930502f7ebafd4b3b496e4a7
Ashpreet <ashpreetbedi@users.noreply.github.com>
2025-03-09 15:02:55 +0000

    MCP Docs

--- cookbook/examples/apps/mcp_agent/test_mcp_manager.py
+++ cookbook/examples/apps/mcp_agent/test_mcp_manager.py
@@ -4,16 +4,37 @@ from mcp_manager import MCPManager
 
 
 def main():
-    test_server_configs = [
+    server_configs = [
         {
             "id": "github",
             "command": "npx",
             "args": ["-y", "@modelcontextprotocol/server-github"],
             "env_vars": {"GITHUB_TOKEN": "GitHub Personal Access Token"},
         },
+        {
+            "id": "filesystem",
+            "command": "npx",
+            "args": ["-y", "@modelcontextprotocol/server-filesystem", "."],
+        },
+        {
+            "id": "memory",
+            "command": "npx",
+            "args": ["-y", "@modelcontextprotocol/server-memory"],
+        },
+        {
+            "id": "git",
+            "command": "uvx",
+            "args": ["mcp-server-git"],
+        },
+        {
+            "id": "brave-search",
+            "command": "npx",
+            "args": ["-y", "@modelcontextprotocol/server-brave-search"],
+            "env_vars": {"BRAVE_API_KEY": "Your Brave API Key"},
+        },
     ]
 
-    mcp_manager = MCPManager(test_server_configs)
+    mcp_manager = MCPManager(server_configs)
     mcp_tools = mcp_manager.get_mcp_tools()
 
     # Print the MCPTools objects for demonstration.
