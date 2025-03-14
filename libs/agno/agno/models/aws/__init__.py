7c3cd253c20f28ffb317dbd584ce3a522b298d27
Michael Pilosov <40366263+mathematicalmichael@users.noreply.github.com>
2025-03-14 13:56:17 -0600

    claude should be optional for bedrock use (#2416)
    per this example:
    https://docs.agno.com/examples/models/aws/bedrock/structured_output - if
    not using claude, shouldn't need to install `anthropic`. `pip install
    agno boto3` should be enough.
    
    ## Description
    
    - **Summary of changes**: Made `anthropic` import optional for bedrock
    - **Related issues**: Mention if this PR fixes or is connected to any
    issues.
    - **Motivation and context**: Tried to run example exactly as documented
    on the website and failed due to missing (unnecessary import)
    - **Environment or dependencies**: 
    - **Impact on metrics**: 
    
    
    ---
    
    ## Type of change
    
    Please check the options that are relevant:
    
    - [X] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Model update (Addition or modification of models)
    - [ ] Other (please describe):
    
    ---
    
    ## Checklist
    
    - [ ] Adherence to standards: Code complies with Agno’s style guidelines
    and best practices.
    - [ ] Formatting and validation: You have run `./scripts/format.sh` and
    `./scripts/validate.sh` to ensure code is formatted and linted.
    - [ ] Self-review completed: A thorough review has been performed by the
    contributor(s).
    - [ ] Documentation: Docstrings and comments have been added or updated
    for any complex logic.
    - [ ] Examples and guides: Relevant cookbook examples have been included
    or updated (if applicable).
    - [ ] Tested in a clean environment: Changes have been tested in a clean
    environment to confirm expected behavior.
    - [ ] Tests (optional): Tests have been added or updated to cover any
    new or changed functionality.
    
    ---
    
    ## Additional Notes
    
    Include any deployment notes, performance implications, security
    considerations, or other relevant information (e.g., screenshots or logs
    if applicable).
    
    ---------
    
    Co-authored-by: Yash Pratap Solanky <101447028+ysolanky@users.noreply.github.com>

--- libs/agno/agno/models/aws/__init__.py
+++ libs/agno/agno/models/aws/__init__.py
@@ -1,2 +1,5 @@
 from agno.models.aws.bedrock import AwsBedrock
-from agno.models.aws.claude import Claude
+try:
+    from agno.models.aws.claude import Claude
+except ImportError:
+    Claude = None
