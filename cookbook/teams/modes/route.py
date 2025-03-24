64249f33dd7a1868835082b9d932d0125395ac8b
Ashpreet <ashpreetbedi@users.noreply.github.com>
2025-03-24 16:16:26 +0000

    README

--- cookbook/teams/modes/route.py
+++ cookbook/teams/modes/route.py
@@ -78,7 +78,6 @@ multi_language_team = Team(
         "For unsupported languages like Italian, respond in English with the above message.",
     ],
     show_members_responses=True,
-    debug_mode=True,
 )
 
 
