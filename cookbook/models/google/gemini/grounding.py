39325b53348b6417dd2f7d8668d95c395fe5af4c
Ashpreet <ashpreetbedi@users.noreply.github.com>
2025-04-02 23:07:06 +0100

    README

--- cookbook/models/google/gemini/grounding.py
+++ cookbook/models/google/gemini/grounding.py
@@ -2,8 +2,8 @@ from agno.agent import Agent
 from agno.models.google import Gemini
 
 agent = Agent(
-    model=Gemini(id="gemini-2.0-flash-exp", grounding=True),
-    show_tool_calls=True,
+    model=Gemini(id="gemini-2.0-flash", grounding=True),
+    add_datetime_to_instructions=True,
     markdown=True,
 )
-agent.print_response("News from USA?")
+agent.print_response("What's the latest on Tariffs?", stream=True)
