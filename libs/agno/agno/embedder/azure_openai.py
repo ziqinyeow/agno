29d15177f97b4a88fba97c292817bf40098e2d1b
SAIKIRAN GONUGUNTA <skiran252@gmail.com>
2025-04-18 01:54:02 +0530

    update: AzureOpenAIEmbedder to consider client params passed in `client_params` arg (#2764)
    ## Summary
    
    Describe key changes, mention related issues or motivation for the
    changes.
    
    ## Type of change
    
    - [ ] Bug fix (If applicable, issue number: #____)
    - [ ] New feature
    - [ ] Breaking change
    - [x] Improvement
    - [ ] Model update
    - [ ] Other: _____________________
    
    ---
    
    ## Checklist
    
    - [ ] Code complies with style guidelines
    - [ ] Ran format/validation scripts (`./scripts/format.sh` and
    `./scripts/validate.sh`)
    - [ ] Self-review completed
    - [ ] Documentation updated (comments, docstrings)
    - [ ] Examples and guides: Relevant cookbook examples have been included
    or updated (if applicable)
    - [ ] Tested in clean environment
    - [ ] Tests added/updated (if applicable)
    
    ---
    
    ## Additional Notes
    
    Add any important context (deployment instructions, screenshots,
    security considerations, etc.)
    
    ---------
    
    Co-authored-by: Dirk Brand <51947788+dirkbrnd@users.noreply.github.com>

--- libs/agno/agno/embedder/azure_openai.py
+++ libs/agno/agno/embedder/azure_openai.py
@@ -55,6 +55,10 @@ class AzureOpenAIEmbedder(Embedder):
             _client_params["azure_ad_token"] = self.azure_ad_token
         if self.azure_ad_token_provider:
             _client_params["azure_ad_token_provider"] = self.azure_ad_token_provider
+        
+        if self.client_params:
+            _client_params.update(self.client_params)
+
         return AzureOpenAIClient(**_client_params)
 
     def _response(self, text: str) -> CreateEmbeddingResponse:
