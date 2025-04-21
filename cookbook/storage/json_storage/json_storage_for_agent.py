a21060d240d979a154c76327e981a011850a7db8
Ashpreet <ashpreetbedi@users.noreply.github.com>
2025-04-20 18:48:59 -0700

    README

--- cookbook/storage/json_storage/json_storage_for_agent.py
+++ cookbook/storage/json_storage/json_storage_for_agent.py
@@ -2,9 +2,11 @@
 
 from agno.agent import Agent
 from agno.storage.json import JsonStorage
+from agno.models.openai import OpenAIChat
 from agno.tools.duckduckgo import DuckDuckGoTools
 
 agent = Agent(
+    model=OpenAIChat(id="gpt-4o-mini"),
     storage=JsonStorage(dir_path="tmp/agent_sessions_json"),
     tools=[DuckDuckGoTools()],
     add_history_to_messages=True,
