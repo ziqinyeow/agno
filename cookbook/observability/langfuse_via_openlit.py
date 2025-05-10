92bfb00a09447accf83022c30fe2f2a239e84b98
Dirk Brand <51947788+dirkbrnd@users.noreply.github.com>
2025-05-10 18:31:51 +0200

    Add cookbook for langfuse via openlit (#3127)
    ## Summary
    
    Basic implementation of Langfuse using OpenLit
    
    ## Type of change
    
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Improvement
    - [ ] Model update
    - [ ] Other:
    
    ---
    
    ## Checklist
    
    - [ ] Code complies with style guidelines
    - [ ] Ran format/validation scripts (`./scripts/format.sh` and
    `./scripts/validate.sh`)
    - [ ] Self-review completed
    - [ ] Documentation updated (comments, docstrings)
    - [ ] Examples and guides: Relevant cookbook examples have been included
    or updated (if applicable)
    - [ ] Tested in clean environment
    - [ ] Tests added/updated (if applicable)
    
    ---
    
    ## Additional Notes
    
    Add any important context (deployment instructions, screenshots,
    security considerations, etc.)

---
+++ cookbook/observability/langfuse_via_openlit.py
@@ -0,0 +1,49 @@
+"""
+This example shows how to use langfuse via OpenLIT to trace model calls.
+
+1. Install dependencies: pip install openai langfuse openlit opentelemetry-sdk opentelemetry-exporter-otlp
+2. Set your Langfuse API key as an environment variables: 
+  - export LANGFUSE_PUBLIC_KEY=<your-key>
+  - export LANGFUSE_SECRET_KEY=<your-key>
+"""
+
+import base64
+import os
+from agno.agent import Agent
+from agno.models.openai import OpenAIChat
+from agno.tools.duckduckgo import DuckDuckGoTools
+
+LANGFUSE_AUTH = base64.b64encode(f"{os.getenv('LANGFUSE_PUBLIC_KEY')}:{os.getenv('LANGFUSE_SECRET_KEY')}".encode()).decode()
+
+os.environ["OTEL_EXPORTER_OTLP_ENDPOINT"]="https://us.cloud.langfuse.com/api/public/otel" # 🇺🇸 US data region
+# os.environ["OTEL_EXPORTER_OTLP_ENDPOINT"]="https://cloud.langfuse.com/api/public/otel" # 🇪🇺 EU data region
+# os.environ["OTEL_EXPORTER_OTLP_ENDPOINT"]="http://localhost:3000/api/public/otel" # 🏠 Local deployment (>= v3.22.0)
+ 
+os.environ["OTEL_EXPORTER_OTLP_HEADERS"]=f"Authorization=Basic {LANGFUSE_AUTH}"
+
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+ 
+trace_provider = TracerProvider()
+trace_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter()))
+ 
+# Sets the global default tracer provider
+from opentelemetry import trace
+trace.set_tracer_provider(trace_provider)
+ 
+# Creates a tracer from the global tracer provider
+tracer = trace.get_tracer(__name__)
+
+import openlit
+# Initialize OpenLIT instrumentation. The disable_batch flag is set to true to process traces immediately.
+openlit.init(tracer=tracer, disable_batch=True)
+
+agent = Agent(
+    model=OpenAIChat(id="gpt-4o-mini"), 
+    tools=[DuckDuckGoTools()],
+    markdown=True, 
+    debug_mode=True,
+)
+
+agent.run("What is currently trending on Twitter?")
