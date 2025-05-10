2c7895049ce5eaa223f095ff85147437f164220b
Dirk Brand <dirkbrnd@gmail.com>
2025-05-11 00:31:30 +0200

    Add example for input as messages

---
+++ cookbook/agent_concepts/other/input_as_messages_list.py
@@ -0,0 +1,16 @@
+from agno.agent import Agent, Message
+
+Agent().print_response(
+    messages=[
+        Message(
+            role="user",
+            content=[{"type": "text", "text": "Hi! My name is John."},],
+        ),
+        Message(
+            role="user",
+            content=[{"type": "text", "text": "What are you capable of?"},],
+        ),
+    ],
+    stream=True,
+    markdown=True,
+)
