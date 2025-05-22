8b517066e36b35831b8dd4ec1e312d4f164f2875
Dirk Brand <dirkbrnd@gmail.com>
2025-05-22 13:09:38 +0200

    Fix firecrawl import

--- libs/agno/agno/tools/firecrawl.py
+++ libs/agno/agno/tools/firecrawl.py
@@ -1,6 +1,6 @@
 import json
 from os import getenv
-from typing import List, Optional
+from typing import List, Optional, Dict, Any
 
 from agno.tools import Toolkit
 from agno.utils.log import logger
