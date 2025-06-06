eee7d7f7e16eb5a346734949d2ea8a3da3d8c1c1
Dirk Brand <dirkbrnd@gmail.com>
2025-06-06 23:30:19 +0200

    Remove infinity reranker import

--- libs/agno/agno/reranker/__init__.py
+++
@@ -1,5 +0,0 @@
-from agno.reranker.base import Reranker
-from agno.reranker.cohere import CohereReranker
-from agno.reranker.infinity import InfinityReranker
-
-__all__ = ["Reranker", "CohereReranker", "InfinityReranker"]
