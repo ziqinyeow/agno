bcecf4981d9e418ddd37f754dd24ec11cd535b0a
SamuelJacobJ <sam@agno.com>
2025-06-11 21:23:24 +0100

    feat: Add Nebius embedding support (#3531)
    ## Summary
    
    Adds embedding support via Nebius
    
    Output of cookbook:
    `Embeddings: [0.0041656494140625, 0.002140045166015625,
    -0.005718231201171875, 0.007724761962890625, -0.01416015625]
    Dimensions: 4096`
    
    (If applicable, issue number: #\_\_\_\_)
    
    ## Type of change
    
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Improvement
    - [ ] Model update
    - [ ] Other:
    
    ---
    
    ## Checklist
    
    - [x] Code complies with style guidelines
    - [ ] Ran format/validation scripts (`./scripts/format.sh` and
    `./scripts/validate.sh`)
    - [x] Self-review completed
    - [ ] Documentation updated (comments, docstrings)
    - [x] Examples and guides: Relevant cookbook examples have been included
    or updated (if applicable)
    - [x] Tested in clean environment
    - [ ] Tests added/updated (if applicable)
    
    ---
    
    ## Additional Notes
    
    Add any important context (deployment instructions, screenshots,
    security considerations, etc.)
    
    Co-authored-by: Samuel <samueljacobjupe@gmail.com>
    Co-authored-by: Dirk Brand <51947788+dirkbrnd@users.noreply.github.com>

---
+++ cookbook/agent_concepts/knowledge/embedders/nebius_embedder.py
@@ -0,0 +1,21 @@
+from agno.agent import AgentKnowledge
+from agno.embedder.nebius import NebiusEmbedder
+from agno.vectordb.pgvector import PgVector
+
+embeddings = NebiusEmbedder().get_embedding(
+    "The quick brown fox jumps over the lazy dog."
+)
+
+# Print the embeddings and their dimensions
+print(f"Embeddings: {embeddings[:5]}")
+print(f"Dimensions: {len(embeddings)}")
+
+# Example usage:
+knowledge_base = AgentKnowledge(
+    vector_db=PgVector(
+        db_url="postgresql+psycopg://ai:ai@localhost:5532/ai",
+        table_name="nebius_embeddings",
+        embedder=NebiusEmbedder(),
+    ),
+    num_documents=2,
+) 
---
+++ libs/agno/agno/embedder/nebius.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from os import getenv
+from typing import Optional
+
+from agno.embedder.openai import OpenAIEmbedder
+
+
+@dataclass
+class NebiusEmbedder(OpenAIEmbedder):
+    id: str = "BAAI/bge-en-icl"
+    dimensions: int = 1024
+    api_key: Optional[str] = getenv("NEBIUS_API_KEY")
+    base_url: str = "https://api.studio.nebius.com/v1/" 
